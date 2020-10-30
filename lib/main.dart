a84ce76d584ad5e4075d6d3b3d12cd7193ae3030
valentynkuts <valik.kuts@gmail.com>
2020-10-30 19:24:32 +0100

    yaml

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,16 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text(
-          'Hello,Valentyn',
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://www.w3schools.com/w3css/img_lights.jpg'),
+          ),
         ),
       ),
     ),
