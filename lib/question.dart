306232d3043a6117d84ec3c699bfb75fd20c45aa
valentynkuts <valik.kuts@gmail.com>
2021-02-19 10:13:07 +0100

    quizzler

---
+++ lib/question.dart
@@ -0,0 +1,16 @@
+class Question{
+  String text;
+  bool answer;
+
+  /*Question({String q, bool a}){
+    text = q;
+    answer = a;
+  }*/
+
+  /*Question(String q, bool a){
+    text = q;
+    answer = a;
+  }*/
+
+  Question(this.text, this.answer);
+}
