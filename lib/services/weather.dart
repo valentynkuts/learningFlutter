bb675422078c0ba33c3664d0df977860405a03e6
valentynkuts <valik.kuts@gmail.com>
2021-03-26 12:39:14 +0100

    Clima

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,36 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+
+const apiKey = '23633a16fbbf6a4d9f39dd86d6f108e4';
+const openWeatherMapURL = 'http://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+
+  Future<dynamic> getCityWeather(String cityName) async{
+    NetworkHelper networkHelper =
+    NetworkHelper('$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+
+    return weatherData;
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+    /*
+    String cityName = 'Gdansk';
+    NetworkHelper networkHelper =
+    NetworkHelper('$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric');
+    */
+    var weatherData = await networkHelper.getData();
+
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
