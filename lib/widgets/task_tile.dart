561132d76726c4260d7bc7f53964ce118a9c4412
valentynkuts <valik.kuts@gmail.com>
2021-04-20 13:00:38 +0200

    changed file task_tile.dart

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,34 +1,52 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+//Global State in order to change TextStyle, TaskCheckbox
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  // void toggleCheckboxState(bool checkboxState) {
+  //   setState(() {
+  //     isChecked = checkboxState;
+  //   });
+  // }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text("This is a task."),
-      trailing: TaskCheckbox(),
+      title: Text(
+        "This is a task.",
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
+      ),
+      trailing: TaskCheckbox(
+        checkboxState: isChecked,
+        //change toggleCheckboxState to anonymous function
+        checkboxCallback: (bool checkboxState) {
+          setState(() {
+            isChecked = checkboxState;
+          });
+        },
+      ),
     );
   }
 }
 
-class TaskCheckbox extends StatefulWidget {
-  @override
-  _TaskCheckboxState createState() => _TaskCheckboxState();
-}
+class TaskCheckbox extends StatelessWidget {
+  final bool checkboxState;
+  final Function checkboxCallback;
 
-class _TaskCheckboxState extends State<TaskCheckbox> {
-  bool isChecked = false;
+  TaskCheckbox({this.checkboxState, this.checkboxCallback});
 
   @override
   Widget build(BuildContext context) {
     return Checkbox(
-      activeColor: Colors.lightBlueAccent,
-      value: isChecked,
-      onChanged: (newValue) {
-        // Local State
-        setState(() {
-          isChecked = newValue;
-        });
-      },
-    );
+        activeColor: Colors.lightBlueAccent,
+        value: checkboxState,
+        onChanged: checkboxCallback);
   }
 }
